c708a75488e04ebf421dc0153626bb79cc84a9bd
lizmckinley <lizmckinley@microsoft.com>
2024-05-19 17:44:41 -0700

    creating a list view and adding swipe delete

--- ChapterApp/MainPage.xaml.cs
+++ ChapterApp/MainPage.xaml.cs
@@ -1,24 +1,13 @@
-﻿namespace ChapterApp
+﻿using ChapterApp.ViewModel;
+
+namespace ChapterApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public MainPage()
+        public MainPage(MainViewModel vm)
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            BindingContext = vm; // Associates the ViewModel with the main page
         }
     }
 
